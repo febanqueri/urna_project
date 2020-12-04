2f06a90aa194b58bf589c3a245bff84887d95573
febanqueri <56729735+febanqueri@users.noreply.github.com>
2020-12-04 16:46:29 -0300

    "WHITE/BRANCO" button

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
 let numero = '';
+let votoBranco = false;
 
 function comecarEtapa(){
     let etapa = etapas[etapaAtual];
     
     let numeroHtml = '';
     numero = '';
+    votoBranco = false;
 
     for(let i=0;i<etapa.numeros;i++){
         if(i === 0){
@@ -73,7 +75,13 @@ function clicou(n){
     }
 }
 function branco(){
-    alert("Clicou em BRANCO!");
+    numero = '';
+    votoBranco = true;
+    seuVotoPara.style.display = 'block';
+    aviso.style.display = 'block';
+    numeros.innerHTML = '';
+    descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
+    
 }
 function corrige(){
     comecarEtapa();
